9dd5ce8ea4691b16bef4670f1d665e9e80ead56e
Charlie <charlesgrimes90@gmail.com>
2016-11-23 23:04:24 +0000

    Implemented Singleton pattern.

---
+++ Code/DesignPatterns/Singleton.cs
@@ -0,0 +1,15 @@
+﻿namespace MiscSnippets.Code.DesignPatterns {
+    public class Singleton {
+        private static Singleton instance;
+
+        private static Singleton Instance {
+            get {
+                if (instance == null) instance = new Singleton();
+
+                return instance;
+            }
+        }
+
+        private Singleton() { }
+    }
+}
