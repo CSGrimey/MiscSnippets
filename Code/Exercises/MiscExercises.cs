8148d38b1c5656ea9809e0e6f0457dae8a37c38a
Charlie <charlesgrimes90@gmail.com>
2016-11-25 22:54:14 +0000

    Wrote multiplication table generator.

---
+++ Code/Exercises/MiscExercises.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace MiscSnippets.Code.Exercises {
+    class MiscExercises {
+        public string MultiplicationTable(int max) {
+            StringBuilder tableStringBuilder = new StringBuilder();
+
+            for (int i = 1; i < max; i++) {
+                for (int j = 1; j < max; j++) {
+                    int multipliedResult = i * j;
+
+                    tableStringBuilder.Append(multipliedResult);
+                }
+
+                tableStringBuilder.Append(Environment.NewLine);
+            }
+
+            return tableStringBuilder.ToString();
+        }
+    }
+}
---
+++ Tests/MiscTests.cs
@@ -0,0 +1,17 @@
+﻿using MiscSnippets.Code.Exercises;
+using NUnit.Framework;
+using System;
+
+namespace MiscSnippets.Tests {
+    [TestFixture]
+    class MiscTests {
+        private readonly MiscExercises exercises = new MiscExercises();
+
+        [Test]
+        public void ShouldCreateMultiplicationTableString() {
+            string expectedResult = $"1234{Environment.NewLine}2468{Environment.NewLine}36912{Environment.NewLine}481216{Environment.NewLine}";
+
+            Assert.AreEqual(expectedResult, exercises.MultiplicationTable(5));
+        }
+    }
+}
