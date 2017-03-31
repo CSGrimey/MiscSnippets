9de46ff248066c3e98820f8c9b93471c2a8d1e91
Charles Grimes <CSGrimey@users.noreply.github.com>
2017-03-31 16:49:22 +0100

    Added recursive merge sort.
    Used the Scala way of doing this as inspiration. 
    Though the way both languages implement the list data type is very different, which has lead to this version being quite inefficient in comparison.

--- Code/Algorithms/MergeSort.cs
+++ Code/Algorithms/MergeSort.cs
@@ -1,5 +1,24 @@
 ﻿namespace MiscSnippets.Code.Algorithms {
-    class MergeSort {
+    public class MergeSorter {
+        public List<T> MergeSort<T>(List<T> values) where T : IComparable<T> {
+            int n = values.Count() / 2;
 
+            if (n == 0) return values;
+            else {
+                List<T> leftSide = values.Take(n).ToList();
+                List<T> rightSide = values.Skip(n).ToList();
+
+                return Merge(MergeSort(leftSide), MergeSort(rightSide));
+            }
+        }
+
+        private List<T> Merge<T>(List<T> leftSide, List<T> rightSide) where T : IComparable<T> {
+            if (leftSide.Any() && rightSide.Any() == false) return leftSide;
+            else if (leftSide.Any() == false && rightSide.Any()) return rightSide;
+            else {
+                if (leftSide.ElementAt(0).CompareTo(rightSide.ElementAt(0)) < 0) return new List<T> { leftSide.ElementAt(0) }.Concat(Merge(leftSide.Skip(1).ToList(), rightSide)).ToList();
+                else return new List<T> { rightSide.ElementAt(0) }.Concat(Merge(rightSide.Skip(1).ToList(), leftSide)).ToList();
+            }
+        }
     }
 }
