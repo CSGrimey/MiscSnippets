c530dc4e3e537a8da370a2273d811bd705d4e1f2
Charles Grimes <CSGrimey@users.noreply.github.com>
2017-04-03 17:21:23 +0100

    Removed recreation of comparison function.

--- Code/Algorithms/InsertionSorter.cs
+++ Code/Algorithms/InsertionSorter.cs
@@ -1,12 +1,13 @@
-public class InsertionSorter : ISorter {
-    public List<T> Sort<T>(List<T> values) where T : IComparable<T> =>
-        values.Aggregate(new List<T>(), (accumulator, currentValue) => Insert(accumulator, currentValue));
-
-    private List<T> Insert<T>(List<T> values, T currentValue) where T : IComparable<T> {
+ public class InsertionSorter : ISorter {
+    public List<T> Sort<T>(List<T> values) where T : IComparable<T> {
         Func<T, T, bool> lessThan = (a, b) => a.CompareTo(b) < 0;
 
-        List<T> leftSide = values.TakeWhile(v => lessThan(v, currentValue)).ToList();
-        List<T> rightSide = values.SkipWhile(v => lessThan(v, currentValue)).ToList();
+        return values.Aggregate(new List<T>(), (accumulator, currentValue) => Insert(accumulator, currentValue, lessThan));
+    }
+
+    private List<T> Insert<T>(List<T> values, T currentValue, Func<T, T, bool> comparisonFunction) where T : IComparable<T> {
+        List<T> leftSide = values.TakeWhile(v => comparisonFunction(v, currentValue)).ToList();
+        List<T> rightSide = values.SkipWhile(v => comparisonFunction(v, currentValue)).ToList();
 
         return leftSide.Concat(new List<T> { currentValue }).Concat(rightSide).ToList();
     }
